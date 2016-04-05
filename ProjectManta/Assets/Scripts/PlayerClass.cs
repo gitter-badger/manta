d52ec76b9733634ce72ff077c0ba3bda2ba155d3
Hugo Santos <hdsantos89@gmail.com>
2016-04-05 16:59:33 +1000

    [Added Project] (Pull Request)
    Added the project to the repository.

---
+++ ProjectManta/Assets/Scripts/PlayerClass.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerClass : MonoBehaviour {
+
+	#region Variables
+
+	//public string username;
+	private int maxHealth;
+	private int curHealth;
+	private int maxEnergy;
+	private int curEnergy;
+	private int damage;
+
+//	private int defence;
+//	private int critChance;
+
+	#endregion
+
+	#region Unity Functions
+
+	// Use this for initialization
+	void Start () {
+		curHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckHealth();
+	}
+
+	#endregion
+
+	#region Created Functions
+
+	public int Health {
+		get { return curHealth; }
+		set { curHealth = value; }
+	}
+
+	public int Damage {
+		get { return damage; }
+		set { damage = value; }
+	}
+
+	public int Energy {
+		get { return curEnergy; }
+		set { curEnergy = value; }
+	}
+
+	void CheckHealth(){
+		if(curHealth > maxHealth){
+			curHealth = maxHealth;
+		}
+		if(curHealth <= 0){
+			//Death Function
+		}
+	}
+
+//	public void TakeDamage(int dam){
+//		int chance = Random.Range(0, 100);
+//		if(chance <= critChance){
+//			curHealth -= dam;
+//		}else{
+//			curHealth -= (int)(((100 - defence) / 100) * dam);
+//		}
+//	}
+
+	#endregion
+
+}

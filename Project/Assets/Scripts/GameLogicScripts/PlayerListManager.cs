6af89389ff8d762a20a7451237dc1c7bd5028cd5
Jeff Tremble <jeff@tremble.biz>
2016-04-06 13:18:17 +1000

    Updated turn Manager and Added Playerlist
    Updated the Turn Manager Script and added a Playerlist (incomplete)
    script that Luke made.

---
+++ Project/Assets/Scripts/GameLogicScripts/PlayerListManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+public class PlayerListManager : NetworkBehaviour
+{
+	public List<NetworkInstanceId> playerList = new List<NetworkInstanceId> ();
+
+	public NetworkManager netManager;
+
+	public bool turnIsActive = false;
+
+	private int currentPlayerNumber = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+		netManager = GameObject.FindWithTag ("NetworkManager").GetComponent<NetworkManager> ();
+	}
+	
+	// Update is called once per frame
+	void OnClientConnected ()
+    {
+
+		CmdAddPlayer (gameObject.GetComponent<NetworkIdentity> ().netId);
+
+	}
+
+	[Command] public void CmdAddPlayer(NetworkInstanceId netId)
+	{
+		playerList.Add (netId);
+	}
+
+
+	[Command] public void CmdTellServerToSwitchTurn() //command that tells all players the turn has been switched to the next player
+	{
+		currentPlayerNumber++;
+
+		RpcSwitchTurn (playerList [currentPlayerNumber]);
+	}
+
+	[ClientRpc] public void RpcSwitchTurn(NetworkInstanceId netid) //
+	{
+		if (gameObject.GetComponent<NetworkIdentity> ().netId == netid)
+		{
+			turnIsActive = true;
+		}
+		else
+		{
+			turnIsActive = false;
+		}
+	}
+}
---
+++ Project/Assets/Scripts/GameLogicScripts/TurnManager.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TurnManager
+{
+    //public NetworkPlayer[] playerList; 
+
+    public int turncounter; 
+
+    public bool char1Turn;
+    public bool char2Turn;
+    public bool char3Turn;
+    public bool char4Turn;
+    public List<NetworkPlayer> players;
+
+    public enum characterTurns
+    {
+        CHARACTER1,
+        CHARACTER2,
+        CHARACTER3,
+        CHARACTER4
+    }
+    public characterTurns currentState;
+
+    void Start()
+    {
+        //List<NetworkPlayer> players = new List<NetworkPlayer> ();
+        //playerList = Network.connections;
+        //print (playerList[0]);
+        turncounter = 1;
+        ChangeTurnToC1();
+        currentState = characterTurns.CHARACTER1;
+    }
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case characterTurns.CHARACTER1:
+                {
+
+                    if (Input.GetKeyDown(KeyCode.Space)) //condition for switching turns
+                    {
+                        turncounter++;
+                        ChangeTurnToC2();
+                        currentState = characterTurns.CHARACTER2;
+                    }
+                }
+                break;
+            case characterTurns.CHARACTER2:
+                {
+                    if (Input.GetKeyDown(KeyCode.Space)) //condition for switching turns
+                    {
+                        turncounter++;
+                        ChangeTurnToC3();
+                        currentState = characterTurns.CHARACTER3;
+                    }
+                }
+                break;
+            case characterTurns.CHARACTER3:
+                {
+
+                    if (Input.GetKeyDown(KeyCode.Space)) //condition for switching turns
+                    {
+                        turncounter++;
+                        ChangeTurnToC4();
+                        currentState = characterTurns.CHARACTER4;
+                    }
+                }
+                break;
+            case characterTurns.CHARACTER4:
+                {
+
+                    if (Input.GetKeyDown(KeyCode.Space)) //condition for switching turns
+                    {
+                        turncounter++;
+                        ChangeTurnToC1();
+                        currentState = characterTurns.CHARACTER1;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ChangeTurnToC1() //function for switching to character 1's turn
+    {
+        char1Turn = true;
+        char2Turn = false;
+        char3Turn = false;
+        char4Turn = false;
+    }
+
+    private void ChangeTurnToC2() //function for switching to character 2's turn
+    {
+        char1Turn = false;
+        char2Turn = true;
+        char3Turn = false;
+        char4Turn = false;
+    }
+
+    private void ChangeTurnToC3() //function for switching to character 3's turn
+    {
+        char1Turn = false;
+        char2Turn = false;
+        char3Turn = true;
+        char4Turn = false;
+    }
+
+    private void ChangeTurnToC4() //function for switching to character 4's turn
+    {
+        char1Turn = false;
+        char2Turn = false;
+        char3Turn = false;
+        char4Turn = true;
+    }
+
+    public bool getBool
+    {
+        get
+        {
+            return char1Turn;
+        }
+        set
+        {
+            char1Turn = value;
+        }
+    }
+}

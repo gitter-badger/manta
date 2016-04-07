83da8bcdd97b8e9fe980846ab939fcaee9dc1586
Jake Crew <jake.crew21@gmail.com>
2016-04-07 17:26:08 +1000

    [combatmanager, TW texture, test anim]
    test anim on jellyfish girl, TW and Que

---
+++ Project/Assets/combatSystem/scripts/combatSystem.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class combatSystem : MonoBehaviour {
+    public Button attbtn;
+    private GameObject player;
+    public GameObject enemy;
+    private RaycastHit hit;
+
+	void Start () {
+        
+	}
+
+    void Update() {
+        //if (enemy != null) {
+        //    attbtn.interactable = true;
+        //} else {
+        //    attbtn.interactable = false;
+        //}
+        selectEnemy();
+    }
+
+    void inflictDamage() {
+        
+    }
+
+    void normAttack() {
+        
+    }
+
+    void selectEnemy() {
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("click");
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit);
+            Debug.Log("mouse down and ray cast " + hit.collider.gameObject.name);
+            Collider _col = hit.collider;
+            if (_col.gameObject.tag == "Player" &&
+                _col.gameObject.layer == LayerMask.NameToLayer("player") &&
+                _col.gameObject.layer != LayerMask.NameToLayer("localPlayer")) {
+                enemy = _col.gameObject;
+                Debug.Log("enemy selected " + _col.gameObject.name);
+            } else if (_col.gameObject.tag == "Player" &&
+                _col.gameObject.layer != LayerMask.NameToLayer("player") &&
+                _col.gameObject.layer == LayerMask.NameToLayer("localPlayer")) {
+                enemy = null;
+            }
+        }
+    }
+}

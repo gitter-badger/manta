2a913fec87c587f6bc905e0a15b1df02878652a7
Ethan <ethwork96@gmail.com>
2016-04-07 15:36:36 +1000

    character dev
    char dev

---
+++ Project/Assets/Scripts/TribalCharacter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TribalCharacter : MonoBehaviour
+{
+
+    public int maxHealth;
+    public int curHealth;
+    public int maxEnergy;
+    public int curEnergy;
+    public int damage;
+   // public int turnInt;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        PassiveAbility();
+	}
+
+    void poisonDamage()
+    {
+
+    }
+
+    void PassiveAbility()
+    {
+        float curH = curHealth;
+        float maxH = maxHealth;
+        float factor = 15 + (1 - (curH / maxH)) * 15;
+        damage = (int)factor;
+    }
+
+}

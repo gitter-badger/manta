ef49928ce7c9a735ab5d8a3b05d44571263bdb3d
ColinSik <colinsiktafe@gmail.com>
2016-04-08 13:15:55 +1000

    animation_v4
    tribal warrior animations implemented

---
+++ Project/Assets/Scripts/TribalWarriorAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TribalWarriorAnimation : MonoBehaviour
+{
+    private Animator animCtrl;
+
+    void Start()
+    {
+        animCtrl = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        animCtrl.SetTrigger("Attack");
+    }
+}
